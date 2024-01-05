8cc8860b0b18ee5da17cc8c7aabb8a60bd687d2b
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-05 16:24:37 +0900

    feat: sumBinary 구현

---
+++ src/main/java/cs01/Calculator.java
@@ -0,0 +1,34 @@
+package cs01;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    public boolean[] sumBinary(boolean[] binary1, boolean[] binary2) {
+        var arrayLength = Math.max(binary1.length, binary2.length) + 1;
+        var sumResult = new boolean[arrayLength];
+        binary1 = Arrays.copyOf(binary1, arrayLength);
+        binary2 = Arrays.copyOf(binary2, arrayLength);
+
+        var carry = false;
+        for (var i = 0; i < arrayLength; i++) {
+            var sum = false;
+
+            var carryCount = 0;
+            carryCount += binary1[i] ? 1 : 0;
+            carryCount += binary2[i] ? 1 : 0;
+            carryCount += carry ? 1 : 0;
+
+            sumResult[i] = binary1[i] ^ binary2[i] ^ carry;
+            carry = carryCount >= 2;
+        }
+
+        // 이진수 덧셈 결과의 맨 앞자리 수가 false인 경우 맨 앞 자리를 제거한다.
+        if (!sumResult[sumResult.length - 1]) {
+            sumResult = Arrays.copyOf(sumResult, sumResult.length - 1);
+        }
+
+        return sumResult;
+    }
+
+}
